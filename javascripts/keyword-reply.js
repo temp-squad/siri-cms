52821096b32d5e1043fe9dec8761f256feabf285
littleroyboy <roiboon.cc@gmail.com>
2020-12-17 08:58:22 +0700

    feat: add dialog & snackbar

--- javascripts/keyword-reply.js
+++ javascripts/keyword-reply.js
@@ -1,8 +1,8 @@
 // ts-check
 
 function KeywordReplyMessage(primaryKeywords, additionalKeywords, message) {
-  this.primaryKeywords = primaryKeywords || [];
-  this.additionalKeywords = additionalKeywords || [];
+  this.primaryKeywords = primaryKeywords;
+  this.additionalKeywords = additionalKeywords;
   this.message = message || "";
 }
 
@@ -22,6 +22,9 @@ new Vue({
       primaryKeywords: null,
       additionalKeywords: [],
       message: "",
+      deleteDialog: false,
+      snackbar: false,
+      snackbarText: "",
     };
   },
 
@@ -30,12 +33,13 @@ new Vue({
       this.mode = mode;
     },
     newMessage() {
-      this.primaryKeywords = [],
-      this.additionalKeywords = [],
-      this.message = "",
-      this.selectedIndex = -1,
-
+      this.primaryKeywords = null;
+      this.additionalKeywords = [];
+      this.message = "";
+      this.selectedIndex = -1;
       this.changeMode(MODE.CREATE);
+
+      this.showSnackBar("Created new keyword reply message");
     },
     selectMessage(i) {
       const replyMessage = this.replyMessages[i];
@@ -43,37 +47,47 @@ new Vue({
         return;
       }
 
-      console.log(replyMessage)
-      
-      this.primaryKeywords = replyMessage.primaryKeywords,
-      this.additionalKeywords = replyMessage.additionalKeywords,
-      this.message = replyMessage.message,
-
-      this.selectedIndex = i,
-
-      this.changeMode(MODE.EDIT);
+      (this.primaryKeywords = replyMessage.primaryKeywords),
+        (this.additionalKeywords = replyMessage.additionalKeywords),
+        (this.message = replyMessage.message),
+        (this.selectedIndex = i),
+        this.changeMode(MODE.EDIT);
     },
-    deleteMessage(i) {
-      this.replyMessages = this.replyMessages.filter((_, index) => index !== i);
+    deleteMessage() {
+      this.replyMessages = this.replyMessages.filter((_, index) => index !== this.selectedIndex);
 
       this.newMessage();
+      this.deleteDialog = false;
+      this.showSnackBar("Deleted auto reply message");
+    },
+    showSnackBar(text) {
+      this.snackbar = true;
+      this.snackbarText = text;
     },
     submit() {
       const newReplyMessage = new KeywordReplyMessage(
         this.primaryKeywords,
         this.additionalKeywords,
-        this.message,
+        this.message
       );
 
       if (this.mode === MODE.CREATE) {
         this.replyMessages.push(newReplyMessage);
         this.newMessage();
+        this.showSnackBar("Created new keyword reply message");
       }
 
       if (this.mode === MODE.EDIT) {
-        this.replyMessages[this.selectedIndex].primaryKeywords = this.primaryKeywords;
-        this.replyMessages[this.selectedIndex].additionalKeywords = this.additionalKeywords;
+        this.replyMessages[
+          this.selectedIndex
+        ].primaryKeywords = this.primaryKeywords;
+        this.replyMessages[
+          this.selectedIndex
+        ].additionalKeywords = this.additionalKeywords;
         this.replyMessages[this.selectedIndex].message = this.message;
+        this.showSnackBar(
+          `Saved keyword reply message “$${this.primaryKeywords}”`
+        );
       }
     },
   },
